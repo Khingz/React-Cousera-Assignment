aaad712518ab010a47dafac1530af18f5fef54c9
khingz <kingsley995mr@gmail.com>
2020-09-20 15:11:16 +0100

    uncontrolled form

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,6 +2,7 @@ import React, { Component} from 'react';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import DishDetails from './DishDetails';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
@@ -58,6 +59,7 @@ renderDish(dish) {
           <Route exact path='/menu' component={() => <Menu dishes = {this.state.dishes}/> } />
           <Route path='/menu/:dishId' component={DishWithId} />
           <Route exact path='/contactus' component={Contact} />
+          <Route exact path='/aboutus' component={() => <About leaders={this.state.leaders} />} />
           <Redirect to='/home' />
         </Switch>
         {/* <Menu dishes = {this.state.dishes} onClick={dishId => this.onDishSelect(dishId)}/>
